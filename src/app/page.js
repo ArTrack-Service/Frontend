d1057019c6355e9b3067fac69fcf452faaa4ca10
2022149023 <jewhale073@gmail.com>
2025-05-31 09:20:46 +0900

    feat: searchbar: floating search bar

--- src/app/page.js
+++ src/app/page.js
@@ -9,16 +9,18 @@ const MapViewer = dynamic(() => import('../components/MapViewer'), { ssr: false 
 const BottomSheet = dynamic(() => import('../components/BottomSheet'), { ssr: false })
 
 export default function Home() {
-  const [searchQuery, setSearchQuery] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
 
-  return (
-    <div className="relative w-full h-screen overflow-hidden flex flex-col">
-      <SearchBar onSearch={setSearchQuery} />
-      <div className="flex-1 relative">
-        <MapViewer />
-      </div>
-      <BottomSheet searchQuery={searchQuery} />
-      <BottomNav activeIndex={0}/>
-    </div>
-  )
+    return (
+        <div className="relative w-full h-screen overflow-hidden flex flex-col">
+            <div className="flex-1 relative">
+                <div className="absolute top-4 z-10 w-full">
+                    <SearchBar onSearch={setSearchQuery} />
+                </div>
+                <MapViewer />
+            </div>
+            <BottomSheet searchQuery={searchQuery} />
+            <BottomNav activeIndex={0}/>
+        </div>
+    )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,20 +16,20 @@ const handleKeyDown = (e) => {
 }
 
 return (
-    <div className="p-4 bg-white shadow-md z-10">
-    <div className="flex items-center border border-gray-300 rounded-lg px-2">
-        <input
-            type="text"
-            placeholder="작품명을 입력하세요"
-            className="flex-1 py-2 px-2 outline-none"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyDown={handleKeyDown}
-        />
-        <button className="text-blue-500 font-semibold" onClick={handleSearch}>
-        검색
-        </button>
-    </div>
+    <div className="p-5 z-10">
+        <div className="flex items-center bg-white backdrop-blur-sm border border-gray-300 rounded-full px-5 shadow-md">
+            <input
+                type="text"
+                placeholder="작품명을 입력하세요"
+                className="flex-1 py-2 px-3 outline-none bg-transparent text-gray-800 placeholder-gray-400"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <button className="text-blue-500 font-semibold" onClick={handleSearch}>
+            검색
+            </button>
+        </div>
     </div>
 )
 }
